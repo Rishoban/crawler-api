2f8349579115f00dfbf11c9356493195a2f64374
Rishoban <y.rishoban@cerexio.com>
2025-07-12 12:32:26 +0200

    Delete Api function is added

---
+++ handler/deleteData.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type DeleteRequest struct {
+	URLs []int `json:"urls"`
+}
+
+// POST /archive-crawlers
+func (v *CrawlerService) ArchiveCrawlers(ctx *gin.Context) {
+	var req DeleteRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request: " + err.Error()})
+		return
+	}
+
+	for _, id := range req.URLs {
+		// Only update object_status to 'Archived'
+		err := UpdateRecord(v.DbConnection, "crawler_url", id, nil, 0, "Archived")
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to archive id: " + string(rune(id))})
+			return
+		}
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Selected records archived successfully."})
+}

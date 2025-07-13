38db65431af876fa8ce8e760b36fa40ea7bdf258
Rishoban <y.rishoban@cerexio.com>
2025-07-13 18:13:29 +0200

    Update object info data when delete api is triggered

--- handler/deleteData.go
+++ handler/deleteData.go
@@ -19,8 +19,14 @@ func (v *CrawlerService) ArchiveCrawlers(ctx *gin.Context) {
 	}
 
 	for _, id := range req.URLs {
+
+		record, err := GetRecordByID(v.DbConnection, "crawler_url", id)
+		if err != nil {
+			continue
+		}
+
 		// Only update object_status to 'Archived'
-		err := UpdateRecord(v.DbConnection, "crawler_url", id, nil, 0, "Archived")
+		err = UpdateRecord(v.DbConnection, "crawler_url", id, record.ObjectInfo, 0, "Archived")
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to archive id: " + string(rune(id))})
 			return
